6cfebbc411bb71fc5d3f5076f981b5cc3ad64869
awasiak-visa <awasiak@visa.com>
2025-07-19 15:38:40 +0200

    edit view added

--- src/main/java/pl/coderslab/users/UserEdit.java
+++ src/main/java/pl/coderslab/users/UserEdit.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class UserEdit extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        getServletContext().getRequestDispatcher("/users/edit.jsp").forward(request, response);
     }
 
     @Override
