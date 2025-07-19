e7d203c01a02b2dfab76a706e3c6cfd09fffcd5a
awasiak-visa <awasiak@visa.com>
2025-07-19 15:43:24 +0200

    show view added

--- src/main/java/pl/coderslab/users/UserShow.java
+++ src/main/java/pl/coderslab/users/UserShow.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class UserShow extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        getServletContext().getRequestDispatcher("/users/show.jsp").forward(request, response);
     }
 
     @Override
