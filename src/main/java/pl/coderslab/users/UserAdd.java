65acafc26c68c2fe2972d3f07f7672368345a499
awasiak-visa <awasiak@visa.com>
2025-07-19 15:45:25 +0200

    add view added

--- src/main/java/pl/coderslab/users/UserAdd.java
+++ src/main/java/pl/coderslab/users/UserAdd.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class UserAdd extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+        getServletContext().getRequestDispatcher("/users/add.jsp").forward(request, response);
     }
 
     @Override
