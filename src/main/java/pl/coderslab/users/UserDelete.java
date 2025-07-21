4971fd37713caaffffffa57fe8c3ff1a30be449a
awasiak-visa <awasiak@visa.com>
2025-07-21 06:50:33 +0200

    delete feature updated

--- src/main/java/pl/coderslab/users/UserDelete.java
+++ src/main/java/pl/coderslab/users/UserDelete.java
@@ -3,19 +3,29 @@ package pl.coderslab.users;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import pl.coderslab.User;
+import pl.coderslab.UserDao;
 
 import java.io.IOException;
 
-@WebServlet("/UserDelete")
+@WebServlet("/user/delete")
 public class UserDelete extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        int id = Integer.parseInt(request.getParameter("id"));
+        UserDao userDao = new UserDao();
+        User user = userDao.read(id);
+        request.setAttribute("user", user);
+        getServletContext().getRequestDispatcher("/users/delete.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        int id = Integer.parseInt(request.getParameter("userToDelete"));
+        UserDao userDao = new UserDao();
+        User user = userDao.read(id);
+        userDao.delete(id);
+        response.sendRedirect("/user/list");
     }
 }
 
---
+++ src/main/webapp/users/delete.jsp
@@ -0,0 +1,26 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html lang="en">
+
+<%@ include file="/header.jsp" %>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800">UsersCRUD</h1>
+    <a href="/user/list" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i class="fas fa-download fa-sm text-white-50"></i>Lista użytkowników</a>
+  </div>
+
+  <form action="/user/delete" method="post">
+    <button name="userToDelete" value="${user.id}" type="submit" class="btn btn-primary">Usuń</button>
+  </form>
+
+</div>
+<!-- /.container-fluid -->
+
+<%@ include file="/footer.jsp" %>
+
+</html>
+
